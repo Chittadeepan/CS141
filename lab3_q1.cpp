206a47e611d492a240d68509523972dff663ddb1
Chittadeepan <42205302+Chittadeepan@users.noreply.github.com>
2018-08-17 12:41:17 +0530

    Add files via upload

---
+++ lab3_q1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+int a=5434;
+float b=3;
+double c=6;
+char d=65;
+bool e=5;
+
+cout<<sizeof (a)<<endl;
+cout<<sizeof (b)<<endl;
+cout<<sizeof (c)<<endl;
+cout<<sizeof (d)<<endl;
+cout<<sizeof (e)<<endl;
+return 0;
+}
---
+++ lab3_q2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+int a=3;
+int b=5;
+int div=a/b;
+int sum=a+b;
+int sub=a-b;
+int mul=a*b;
+int rem=a%b;
+cout<<"result"<<div<<endl;
+cout<<"result"<<sum<<endl;
+cout<<"result"<<sub<<endl;
+cout<<"result"<<mul<<endl;
+cout<<"result"<<rem<<endl;
+return 0;
+}
+
