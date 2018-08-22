a84977c99664bfe0741d2e4096593cfd1c4bd022
Chittadeepan <42205302+Chittadeepan@users.noreply.github.com>
2018-08-22 16:01:10 +0530

    Add files via upload

--- lab4_q2.cpp
+++ lab4_q2.cpp
@@ -1,9 +1,11 @@
 #include<iostream>
 using namespace std;
 int main() {
+//write temp in celsius//
 int temp=0;
 int result;
 cout<<"temp=0"<<"°C"<<endl;
+//convert temp into fahrenheit//
 cout<<"result="<<(9.0/5.0)*temp+32<<"°F"<<endl;
 return 0;
 }
