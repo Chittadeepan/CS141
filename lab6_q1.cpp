109e90ea573a9b491b9e27460eae2fa0ffdd2c54
Chittadeepan <42205302+Chittadeepan@users.noreply.github.com>
2018-09-12 16:24:42 +0530

    Add files via upload

--- lab6_q1.cpp
+++ lab6_q1.cpp
@@ -28,13 +28,18 @@ int appname(int unitCst,int units,int taxRt,int salestax,int totdue){
     totdue=salestax+(unitCst*units);
    return totdue;
    }
+int getvalues(int unitCst,int units,int unitsPurch){
+   unitsPurch=unitCst*units;
+   return unitsPurch;
+    }
 //Write a main function with variables called unitCost, unitsPurch, taxRate, salesTax, and totalDue. Your program should call the functions listed above.
 
-int  main(){
-int unitCst=returnsvalue(1);int units=returnsvalue(2);int taxRt=returnsvalue(3);int salestax=0;int totdue=0;
+int main(){
+int unitCst=returnsvalue(1);int units=returnsvalue(2);int taxRt=returnsvalue(3);int salestax=0;int totdue=0;int unitsPurch=0;
  
 salestax=receivevalues(unitCst,units,taxRt,salestax,totdue);
 
+cout<<"units purchase= " <<getvalues(unitCst, units,unitsPurch)<<endl;
 cout<<"salestax= "<<salestax<<endl;
 cout<<"tot due= "<<appname(unitCst,units,taxRt,salestax,totdue)<<endl;
 
