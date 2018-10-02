29d7eaac3ccc0365b82d2cf83e2a81e6f3b40a05
Chittadeepan <42205302+Chittadeepan@users.noreply.github.com>
2018-10-02 15:29:17 +0530

    Update lab6.5_q3.cpp

--- lab6.5_q3.cpp
+++ lab6.5_q3.cpp
@@ -6,7 +6,7 @@ for(long a=0;a<32767;a++){
  long i;
  i=(a*(a+1))/2;
 	for(long b=0;b<32767;b++){
-     if(i==b*b){
+     if(i==b*b&&i>1225){
       cout<<"The next number is"<<i<<endl;
                }
         }
